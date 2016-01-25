1a290fb3d83c7339b3542af0e711bcd6357f11bb
wchang28 <wchang28@hotmail.com>
2016-01-24 20:37:21 -0500

    first commit

--- test/test_query.js
+++ test/test_query.js
@@ -3,6 +3,11 @@ var $ = require('jquery-no-dom');
 //var queryString = '8803 N';
 var queryString = '9106 W';
 
+var start = new Date().getTime();
+
 $.get('http://127.0.0.1/client/query/?queryString='+encodeURIComponent(queryString), function(data){
+	var end = new Date().getTime();
+	console.log('duration (ms):' +(end-start).toString());
+	console.log('');
 	console.log(data);
 });
